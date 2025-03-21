1214cbf31b182a8fdc34d072897c52580fd8fcaf
vvryw <weeraya.watchara@gmail.com>
2025-03-21 22:53:14 +0700

    edit word

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -38,15 +38,14 @@ export default function Banner() {
             }
 
             <div className={styles.bannerText}>
-                <h1 className='text-4xl font-medium'>where every event finds its venue</h1>
-                <h4 className='text-lg font-serif'>Finding the perfect venue has never been easier. Whether it's a wedding, corporate event, or private party, we connecting people to the perfect place.
-                </h4>
+                <h1 className='text-4xl font-medium'>Relax, Rejuvenate, and Reserve with Ease</h1>
+                <h4 className='text-lg font-serif'>your perfect massage experience starts here</h4>
             </div>
             <button className='bg-white text-cyan-600 border border-cyan-600
                 font-semibold py-2 px-2 m-2 rounded z-30 absolute bottom-0 right-0
                 hover:bg-cyan-600 hover:text-white hover:border-transparent'
                 onClick={ (e)=>{ e.stopPropagation(); router.push('/venue') } }> 
-                Select Venue
+                Select Massage Shop
             </button>
         </div>
     );
