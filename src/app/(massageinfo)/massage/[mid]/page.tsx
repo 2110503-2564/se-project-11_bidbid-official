fe12b88d0499199c311df06ad802e773aa429d33
vvryw <weeraya.watchara@gmail.com>
2025-03-23 20:58:52 +0700

    add button to reserve

--- src/app/(massageinfo)/massage/[mid]/page.tsx
+++ src/app/(massageinfo)/massage/[mid]/page.tsx
@@ -1,34 +1,66 @@
-import Image from "next/image";
-import getVenue from "@/libs/getVenue";
-import getMassageShop from "@/libs/getMassageShop";
+'use client'
 
-export default async function MassageDetailPage ( {params} : {params: {mid:string}} ) {
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import Image from 'next/image'
+import getMassageShop from '@/libs/getMassageShop'
 
-    // const venueDetail = await getVenue(params.vid)
-    const massageDetail = await getMassageShop(params.mid)
+export default function MassageDetailPage({ params }: { params: { mid: string } }) {
+  const router = useRouter()
+  const [massage, setMassage] = useState<any>(null)
+  const [loading, setLoading] = useState(true)
 
+  useEffect(() => {
+    const fetchMassage = async () => {
+      try {
+        const res = await getMassageShop(params.mid)
+        setMassage(res.data)
+      } catch (error) {
+        console.error('Failed to fetch massage shop:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
 
-    return (
-        <main className="text-center p-5">
-            <h1 className="text-lg font-medium">{massageDetail.data.name}</h1>
-            <div className="flex flex-row my-5">
-                <Image src={ massageDetail.data.picture }
-                    alt='Massage Image'
-                    width={0} height={0} sizes="100vw"
-                    className="rounded-lg w-[30%]" />
-                <div className="text-medium mx-5 text-left">
-                <div className="text-medium mx-5">Name: { massageDetail.data.name }</div>
-                <div className="text-medium mx-5">Adress: { massageDetail.data.address }</div>
-                <div className="text-medium mx-5">Price Range: { massageDetail.data.priceRange }</div>
-                <div className="text-medium mx-5">Tel: { massageDetail.data.phoneNumber }</div>
-                <div className="text-medium mx-5">Open Time: { massageDetail.data.openTime }</div>
-                <div className="text-medium mx-5">Close Time: { massageDetail.data.closeTime }</div>
-                </div>
-            </div>
-        </main>
-    )
+    fetchMassage()
+  }, [params.mid])
+
+  if (loading) {
+    return <div className="text-center p-5">Loading...</div>
+  }
+
+  if (!massage) {
+    return <div className="text-center p-5 text-red-500">Massage shop not found.</div>
+  }
+
+  return (
+    <main className="text-center p-5">
+      <h1 className="text-lg font-medium">{massage.name}</h1>
+      <div className="flex flex-row my-5">
+        <Image
+          src={massage.picture}
+          alt="Massage Image"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="rounded-lg w-[30%]"
+        />
+        <div className="text-medium mx-5 text-left">
+          <div className="text-medium mx-5">Name: {massage.name}</div>
+          <div className="text-medium mx-5">Address: {massage.address}</div>
+          <div className="text-medium mx-5">Price Range: {massage.priceRange}</div>
+          <div className="text-medium mx-5">Tel: {massage.phoneNumber}</div>
+          <div className="text-medium mx-5">Open Time: {massage.openTime}</div>
+          <div className="text-medium mx-5">Close Time: {massage.closeTime}</div>
+          <button
+            onClick={() => router.push('/reservation')}
+            className="mx-5 my-3 bg-blue-600 text-white px-3 py-1 rounded 
+              hover:bg-blue-700 active:bg-blue-700 active:scale-95"
+          >
+            Reserve NOW!
+          </button>
+        </div>
+      </div>
+    </main>
+  )
 }
-
-// export async function generateStaticParams () {
-//     return [ {vid:'001'} , {vid:'002'} , {vid:'003'} ]
-// }   
